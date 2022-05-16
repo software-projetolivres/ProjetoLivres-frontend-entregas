92de0adab0c6ecccad5fa64530b7264531c1c856
Gabrielle Sperandeo <gabi.sperandeo@hotmail.com>
2022-05-16 20:21:13 -0300

    ajs

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const PORT = process.env.PORT || 5000
 const http = require('http')
 const cadastro = require("./routes/cadastro")
 const login = require("./routes/login")
+const request = require('request')
 
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
-  .get('/', (req, res) => res.sendFile(path.join(__dirname, '../node-js-getting-started/views/pages', 'index.html')))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, '../livres-entregas-fe/views/pages', 'index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  app.use("/cadastro", cadastro)
+  /*app.use("/cadastro", cadastro)
   app.use("/login", login)
 
   app.get('/pokemon', async(req, res) => {
@@ -29,4 +30,4 @@ app
 http.createServer(function (req, res) {
     console.log('request received');
     res.writeHead(200, {'Content-Type': 'text/html'});
-})
+})*/
--- routes/cadastro.js
+++ routes/cadastro.js
@@ -4,7 +4,7 @@ let router = express.Router()
 router
     .route("/")
     .get( (req, res) =>{
-        res.sendFile('/Users/Gabrielle/node-js-getting-started/views/pages/cadastro.html')
+        res.sendFile('/Users/gabis/livres-entregas-fe/views/pages/cadastro.html')
     })
 
 module.exports = router
