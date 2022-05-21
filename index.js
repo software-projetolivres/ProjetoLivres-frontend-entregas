6f8be8d1a609ad086d1f316400795904718709de
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-21 00:19:18 -0300

    tst route

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const cadastro = require("./routes/cadastro")
+/*const cadastro = require("./routes/cadastro")
 const login = require("./routes/login")
 const cadastroCompleto = require("./routes/cadastro-completo")
 const entregas = require("./routes/entregas")
 const minhasEntregas = require("./routes/minhas-entregas")
-const entregasInvalidas = require("./routes/entregas-invalidas")
+const entregasInvalidas = require("./routes/entregas-invalidas")*/
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -16,9 +16,9 @@ app
   .get('/', (req, res) => res.sendFile('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.use("/cadastro", cadastro)
+/*app.use("/cadastro", cadastro)
 app.use("/login", login)
 app.use("/cadastro-completo", cadastroCompleto)
 app.use("/entregas", entregas)
 app.use("/minhas-entregas", minhasEntregas)
-app.use("/entregas-invalidas", entregasInvalidas)
+app.use("/entregas-invalidas", entregasInvalidas)*/
