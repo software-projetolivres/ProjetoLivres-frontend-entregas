28761c1a3fce78e4d48a42c4eb9000ad033b7d7c
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-23 16:21:31 -0300

    refatorando + add coments

---
+++ public/scripts/pages-entregas-scripts.js
@@ -0,0 +1,55 @@
+/* função responsável pela checagem do formato inserido da data nas páginas: /entregas, /minhas-entregas, /entregas-invalidas */
+function checkDate() {
+    var dataEntrega = document.getElementById('data_entrega').value
+    const regex = /^\d{4}-\d{2}-\d{2}$/;
+  
+    if (dataEntrega.match(regex) === null) {
+        document.getElementById('message').innerHTML = '<b><i>A data deve ser inserida no formato AAAA-MM-DD.</i></b>';
+        document.getElementById('entregas_invalidas').disabled = true;
+    }
+  
+    const date = new Date(dataEntrega);
+    const timestamp = date.getTime();
+  
+    if (typeof timestamp !== 'number' || Number.isNaN(timestamp)) {
+        document.getElementById('message').innerHTML = '<b><i>A data deve ser inserida no formato AAAA-MM-DD.</i></b>';
+        document.getElementById('entregas_invalidas').disabled = true;
+    } else{
+        document.getElementById('message').innerHTML = '';
+        document.getElementById('entregas_invalidas').disabled = false;
+    }
+}
+
+/* função responsável por retornar os ids das checkboxes (= ids das entregas) que foram selecionadas nas páginas: /entregas, /minhas-entregas */
+function getIdsSelecionados() {
+    var idsSelecionados = []
+
+    var inputs = document.querySelectorAll('.checkbox');   
+    for (var i = 0; i < inputs.length; i++) {   
+        if(inputs[i].checked == true){
+            idsSelecionados.push(inputs[i].id)
+        }
+    }
+
+    return idsSelecionados;
+}
+
+/* funçao responsável por realizar a chamada da api que atualiza os dados passados */
+async function atualizarDados(putData) {
+    var url = 'https://livres-entregas.herokuapp.com/api/entregas/atualizar';
+
+    try {
+        let res = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Authorization":"Basic " + sessionStorage.getItem('authorization'),
+                "Content-type":"application/json;charset=UTF-8"
+            },
+            body: putData
+          });
+
+          mostrarEntregas();
+    } catch (error) {
+        console.log(error);
+    } 
+}
