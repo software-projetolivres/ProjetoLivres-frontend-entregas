ad84b1a4bd997e9223aeb9edc456c627f9088913
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 17:23:21 -0300

    tst

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,8 @@ let router = express.Router()
 router
     .route("/")
     .get( (req, res) =>{
-        res.sendFile('./../views/pages/login.html')
+        //res.sendFile('./../views/pages/login.html')
+        res.sendFile('/views/pages/login.html')
         //res.sendFile('/Users/Gabrielle/node-js-getting-started/views/pages/login.html')
     })
 
