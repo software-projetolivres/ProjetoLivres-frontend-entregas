d250b2226441d0df7acab892273e1bc29f4eadf8
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 17:32:39 -0300

    tst

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cadastro = require("./routes/cadastro")
-const login = require("./routes/login")
+//const login = require("./routes/login")
 const cadastroCompleto = require("./routes/cadastro-completo")
 const entregas = require("./routes/entregas")
 const minhasEntregas = require("./routes/minhas-entregas")
@@ -16,8 +16,10 @@ app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.use("/cadastro", cadastro)
-app.use("/login", login)
+//app.use("/login", login)
 app.use("/cadastro-completo", cadastroCompleto)
 app.use("/entregas", entregas)
 app.use("/minhas-entregas", minhasEntregas)
 app.use("/entregas-invalidas", entregasInvalidas)
+
+app.get('/login', (req, res) => res.sendFile(__dirname + '/views/pages/login.html'))
