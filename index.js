942b2343c5690e9097d8e1602b0bd86c569f678a
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 17:47:58 -0300

    roteamento

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var cors = require('cors')
 
+app.use(require('./cors'))
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -16,5 +16,3 @@ app.get('/cadastro-completo', (req, res) => res.sendFile(__dirname + '/views/pag
 app.get('/entregas', (req, res) => res.sendFile(__dirname + '/views/pages/entregas.html'))
 app.get('/entregas-invalidas', (req, res) => res.sendFile(__dirname + '/views/pages/entregas-invalidas.html'))
 app.get('/minhas-entregas', (req, res) => res.sendFile(__dirname + '/views/pages/minhas-entregas.html'))
-
-app.use(cors())
