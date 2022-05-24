f486dbfde1f9fd2305732055455a5ecaf8940f96
Gabrielle Sperandeo <gabrielle@inpulso.com.br>
2022-05-24 17:43:07 -0300

    roteamento

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors');
 const PORT = process.env.PORT || 5000
+var cors = require('cors')
 
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .get('/', (req, res) => res.sendFile(__dirname + '/views/pages/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  .use(cors())
 
 app.get('/login', (req, res) => res.sendFile(__dirname + '/views/pages/login.html'))
 app.get('/cadastro', (req, res) => res.sendFile(__dirname + '/views/pages/cadastro.html'))
@@ -17,3 +16,5 @@ app.get('/cadastro-completo', (req, res) => res.sendFile(__dirname + '/views/pag
 app.get('/entregas', (req, res) => res.sendFile(__dirname + '/views/pages/entregas.html'))
 app.get('/entregas-invalidas', (req, res) => res.sendFile(__dirname + '/views/pages/entregas-invalidas.html'))
 app.get('/minhas-entregas', (req, res) => res.sendFile(__dirname + '/views/pages/minhas-entregas.html'))
+
+app.use(cors())
